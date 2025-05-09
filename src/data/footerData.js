ca945001a6a079d6814e99726d1321fdb6add20f
ChristianW$ <christianwyterlin@gmail.com>
2025-05-08 23:49:52 -0300

    Added social media link

--- src/data/footerData.js
+++ src/data/footerData.js
@@ -1,24 +1,27 @@
 const footerData = {
   /*Sobre*/
-  title: '</> ChristianW$',
-  description: 'Conectando código, café e criatividade.',
+  title: "</> ChristianW$",
+  description: "Conectando código, café e criatividade.",
   /*Links*/
-  resourcesTitle: 'Recursos',
+  resourcesTitle: "Recursos",
   /*Recursos*/
   resources: [
-    { name: 'Documentação', url: '#' },
-    { name: 'Repositórios', url: '#' },
-    { name: 'Tutoriais', url: '#' },
-    { name: 'Blog Dev', url: '#' },
+    { name: "Documentação", url: "#" },
+    { name: "Repositórios", url: "#" },
+    { name: "Tutoriais", url: "#" },
+    { name: "Blog Dev", url: "#" },
   ],
-  contactsTitle: 'Contato',
+  contactsTitle: "Contato",
   /*Contatos*/
   contacts: [
-    { name: 'Linkedin', url: '#' },
-    { name: 'GitHub', url: '#' },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/christian-wyterlin-silveira",
+    },
+    { name: "GitHub", url: "https://github.com/Wyterlin" },
   ],
   /*Rodapé*/
-  footerBottom: 'ChristianW$ — Todos os direitos reservados.',
+  footerBottom: "ChristianW$ — Todos os direitos reservados.",
 };
 
 export default footerData;
